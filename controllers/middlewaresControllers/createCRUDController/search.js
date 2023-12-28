61a89f62ff132a968e94fe39f31620a31b76c092
Abhishek jadon <abhisheksode001@gmail.com>
2023-12-28 17:45:59 +0530

    Abhy

--- controllers/middlewaresControllers/createCRUDController/search.js
+++ controllers/middlewaresControllers/createCRUDController/search.js
@@ -1,5 +1,4 @@
 const search = async (Model, req, res) => {
-  // console.log(req.query.fields)
   if (req.query.q === undefined || req.query.q.trim() === '') {
     return res
       .status(202)
@@ -7,27 +6,48 @@ const search = async (Model, req, res) => {
         success: false,
         result: [],
         message: 'No document found by this request',
+        count: 0, // Add count property with value 0
       })
       .end();
   }
-  const fieldsArray = req.query.fields
-    ? req.query.fields.split(',')
-    : ['name', 'surname', 'birthday'];
+
+  // Specify the fields you want to search in
+  const searchFields = [
+    { field: 'customfields.counselor_email', message: 'Counselor Email' },
+    { field: 'contact.email', message: 'Contact Email' },
+    { field: 'contact.phone', message: 'Contact Phone', type: 'number' },
+    { field: 'full_name', message: 'Full Name' },
+    { field: 'lead_id', message: 'Lead ID' },
+    { field: 'customfields.institute_name', message: 'Institute Name' },
+    { field: 'customfields.university_name', message: 'University Name' },
+  ];
 
   const fields = { $or: [] };
 
-  for (const field of fieldsArray) {
+  // Build the $or condition for each search field
+  for (const { field, message, type } of searchFields) {
+    if (type === 'number' && isNaN(Number(req.query.q))) {
+      continue; // Skip the field if it's expected to be a number, but the search term is not a valid number
+    }
+
     fields.$or.push({ [field]: { $regex: new RegExp(req.query.q, 'i') } });
   }
-  // console.log(fields)
+
   try {
+    // Fetch documents based on the specified fields
     let results = await Model.find(fields).where('removed', false).limit(10);
+
+    const count = await Model.countDocuments(fields).where('removed', false);
 
     if (results.length >= 1) {
       return res.status(200).json({
         success: true,
         result: results,
-        message: 'Successfully found all documents',
+        message: `Successfully found all documents in ${searchFields
+          .filter(({ field }) => fields.$or.some((condition) => condition[field]))
+          .map(({ message }) => message)
+          .join(', ')}`,
+        count: count, // Include the count in the response
       });
     } else {
       return res
@@ -36,6 +56,7 @@ const search = async (Model, req, res) => {
           success: false,
           result: [],
           message: 'No document found by this request',
+          count: 0, // Set count to 0 when no documents are found
         })
         .end();
     }
@@ -45,6 +66,7 @@ const search = async (Model, req, res) => {
       result: null,
       message: error.message,
       error: error,
+      count: 0, // Set count to 0 in case of an error
     });
   }
 };
