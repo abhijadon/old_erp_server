bd1f87371b25691008570c99d92b19efc1c3af77
Abhishek jadon <abhisheksode001@gmail.com>
2023-12-30 14:19:04 +0530

    abhishel

--- controllers/coreControllers/adminController/paginatedList.js
+++ controllers/coreControllers/adminController/paginatedList.js
@@ -2,46 +2,39 @@ const mongoose = require('mongoose');
 const Admin = mongoose.model('Admin');
 
 const paginatedList = async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
+  const page = req.query.page || 1;
   const limit = parseInt(req.query.items) || 10;
-  const skip = (page - 1) * limit;
+  const skip = page * limit - limit;
 
-  try {
-    const [results, count] = await Promise.all([
-      Admin.find({ removed: false })
-        .skip(skip)
-        .limit(limit)
-        .sort({ created: 'desc' })
-        .populate()
-        .exec(),
-      Admin.countDocuments({ removed: false }),
-    ]);
+  //  Query the database for a list of all results
+  const resultsPromise = Admin.find({ removed: false, enabled: true })
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' })
+    .populate();
+  // Counting the total documents
+  const countPromise = Admin.countDocuments({ removed: false });
+  // Resolving both promises
+  const [result, count] = await Promise.all([resultsPromise, countPromise]);
+  // Calculating total pages
+  const pages = Math.ceil(count / limit);
 
-    const totalPages = Math.ceil(count / limit);
-    const pagination = { page, totalPages, totalItems: count };
-
-    if (results.length > 0) {
-      results.forEach((admin) => {
-        admin.password = undefined;
-        admin.loggedSessions = undefined;
-      });
-
-      return res.status(200).json({
-        success: true,
-        results,
-        pagination,
-        message: 'Successfully found documents',
-      });
-    } else {
-      return res.status(204).json({
-        success: true,
-        results: [],
-        pagination,
-        message: 'Collection is Empty',
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({ success: false, results: [], message: error.message });
+  // Getting Pagination Object
+  const pagination = { page, pages, count };
+  if (count > 0) {
+    return res.status(200).json({
+      success: true,
+      result,
+      pagination,
+      message: 'Successfully found all documents',
+    });
+  } else {
+    return res.status(203).json({
+      success: false,
+      result: [],
+      pagination,
+      message: 'Collection is Empty',
+    });
   }
 };
 
