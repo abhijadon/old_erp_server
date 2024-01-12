b82bdc9e3686dae0e062cb58da9ba1a9d3b05247
root <root@ubuntu-cloudpanel.localhost>
2024-01-12 06:48:44 +0000

    aB

--- app.js
+++ app.js
@@ -24,17 +24,12 @@ app.use(cors(corsOptions));
 // putting cors as first in order to avoid unneccessary req uests from unallowed origins
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 // app.use(express.static(path.join(__dirname, './client/dist')));
-// app.get('/', function (req, res) {
-//   res.status(200).send('This Project is live and Workin fine🚀🚀🚀🚀');
-// });
+ app.get('/', function (req, res) {
+   res.status(200).send('This Project is live and Workin fine🚀🚀🚀🚀');
+ });
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../frontend', 'dist')));
-// Enable CORS for all routes
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-});
+
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 
 // Takes the raw requests and turns them into usable properties on req.body
